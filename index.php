8a2893d89d853978a7482dcef5c1245aada62c71
blat <blatiere@gmail.com>
2011-07-11 20:52:30 +0200

    fix css homepage

--- index.php
+++ index.php
@@ -4,16 +4,16 @@
 	<div id="primary" class="twocol-stories">
 		<div class="inside">
 			<?php if (have_posts()) : ?>
-				<?php $first = true; ?>
+				<?php $i = 0; ?>
 				<?php while (have_posts()) : the_post(); ?>
-					<div class="story<?php if($first == true) echo " first" ?>">
+					<div class="story <?php echo ($i % 2) ? "right" : "left" ?>">
 						<h3><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php printf(__('Permanent Link to %s', 'hemingway'), get_the_title()); ?>"><?php the_title(); ?></a></h3>
 						<?php the_excerpt() ?>
 						<div class="details">
 							<?php printf(__('Posted at %s', 'hemingway'), get_the_time(__('ga \o\n n/j/y', 'hemingway'))); ?> | <a href="<?php !get_comments_number() && !comments_open() && !pings_open() ? the_permalink() : comments_link(); ?>"><?php comments_number(__('No comments', 'hemingway'),__('1 Comment', 'hemingway'),__('% Comments', 'hemingway')); ?></a> | <?php printf(__('Filed Under: %s', 'hemingway'), get_the_category_list(', ')); ?> | <?php if (is_callable('the_tags')) the_tags(__('tagged', 'hemingway') . ' ', ', ', ' | '); ?> <span class="read-on"><a href="<?php the_permalink() ?>"><?php _e('read on', 'hemingway'); ?></a></span>
 						</div>
 					</div>
-					<?php $first = false; ?>
+					<?php $i++; ?>
 				<?php endwhile; ?>
 		</div>
 				
